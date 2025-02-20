0c1cd1afc0dcf70ee846cff4fcd3f436e80286e1
Ramratan Gupta <ramratan.gupta@gmail.com>
2025-02-20 22:07:14 +0530

    build

--- build.sh
+++ build.sh
@@ -2,11 +2,14 @@
 #!/bin/bash
 
 # Install Node.js dependencies
-cd scraper
+cd frontend
 npm install
-
+npm run build
+cd ../scraper
+npm install
 # Install Python dependencies
 pip3 install --user redis python-dotenv google-generativeai
-
+cd ../api
+npm install
 # Return to root
 cd ..
